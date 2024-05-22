1230e0d603eda7a9134c1dd47975f5706c42f8f7
BrianErikson <briankerikson@gmail.com>
2024-05-22 11:37:44 -0600

    bring local website in line with remote somewhat

--- web/app.py
+++ web/app.py
@@ -2,9 +2,10 @@
 
 from flask import Flask
 
-app = Flask(__name__)
-app.static_folder = 'static'
+app = Flask(__name__, 
+            static_url_path='', 
+            static_folder='static')
 
 @app.route('/')
 def root():
-    return app.send_static_file('root.html')
+    return app.send_static_file('index.html')
