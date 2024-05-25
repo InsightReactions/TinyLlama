71bdcf7cdc2b5cabf76307c7ab03c0034559c580
BrianErikson <briankerikson@gmail.com>
2024-05-24 20:03:57 -0600

    Fix website cutoff on mobile

---
+++ debian/deploy.sh
@@ -0,0 +1,17 @@
+# Deploy first builds the packages, then adds them to the debian repository
+set -eu
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_ROOT="$(cd "$(dirname "$SCRIPT_DIR/../..")" && pwd)"
+DEB_REPO_ROOT="$REPO_ROOT/../debian.insightreactions.github.io"
+
+./build.sh
+
+cd $DEB_REPO_ROOT
+
+# Push new build
+cp "$REPO_ROOT/build/debian"/*.deb "$DEB_REPO_ROOT/docs/pool/main/"
+./build.sh
+git add --all
+git commit -m "updated repository"
+git push
