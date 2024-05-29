f32a361d2d8c963ccc50e68d5109dc61ae0deae0
BrianErikson <briankerikson@gmail.com>
2024-05-29 13:50:16 -0600

    Fix up mobile view

---
+++ web/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [ ! -d "$SCRIPT_DIR/venv" ]; then
+    python3 -m venv $SCRIPT_DIR/venv
+fi
+
+source $SCRIPT_DIR/venv/bin/activate
+pip install -r $SCRIPT_DIR/requirements.txt
+flask run --host=0.0.0.0
