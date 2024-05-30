ace9e8d2ad0d12e0e33d2b63a664597102cb7334
BrianErikson <briankerikson@gmail.com>
2024-05-30 10:50:27 -0600

    basic website update hook

--- web/app.py
+++ web/app.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+import subprocess
+from flask import Flask, jsonify
 
 app = Flask(__name__, 
             static_url_path='', 
             static_folder='static')
+
+@app.route('/hasUpdates', methods=['GET'])
+def has_updates():
+    output = subprocess.check_output(["apt", "update"]).decode("utf-8")
+    print(output)
+    if " can be upgraded." in output:
+        return jsonify(hasUpdates=True)
+    else:
+        return jsonify(hasUpdates=False)
+
+@app.route('/upgrade', methods=['POST'])
+def upgrade():
+    output = subprocess.check_output(["apt", "upgrade", "-y"]).decode("utf-8")
+    print(output)
+    return jsonify(log=output)
 
 @app.route('/')
 def root():
