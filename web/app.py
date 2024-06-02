33a54c9761332d1a7e870400225b38007cd60515
BrianErikson <briankerikson@gmail.com>
2024-06-02 16:12:21 -0600

    auto-update mechanism working, init patchnotes

--- web/app.py
+++ web/app.py
@@ -1,26 +1,127 @@
 #!/usr/bin/env python3
-
 import subprocess
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+import os
+from datetime import datetime, timezone, timedelta
+#import logging
+
+#logging.basicConfig(level=logging.DEBUG)
+
+# Maximum amount of patchnotes to serve to the client at once
+PATCHNOTE_LIMIT = 10
+
 
 app = Flask(__name__, 
             static_url_path='', 
             static_folder='static')
 
-@app.route('/hasUpdates', methods=['GET'])
+
+def check_pid_running(pid):
+    # Use pgrep to search for a process with the given PID
+    result = subprocess.run(['ps', '-q', str(pid), '-o', 'comm='], stdout=subprocess.PIPE)
+
+    # If pgrep finds a matching process, return True
+    if result.stdout:
+        return True
+
+    # Otherwise, return False
+    return False
+
+
+def get_patchnote_files(since: datetime):
+    share_dir = '/etc/tinyllama/patchnotes.d/'  # Added a trailing slash to ensure correct path concatenation
+    filepaths = []
+    creation_times = []
+
+    if not os.path.exists(share_dir):
+        print(f"Directory '{share_dir}' does not exist.")
+        return filepaths, creation_times
+
+    for filename in os.listdir(share_dir):
+        filepath = os.path.join(share_dir, filename)  # Use os.path.join to ensure correct path concatenation
+        if os.path.isfile(filepath):
+            # Get the creation time of the file
+            creation_time = datetime.fromtimestamp(os.stat(filepath).st_ctime, tz=timezone.utc)
+
+            # Strip off microsconds from the creation_time (since they are not supported by the client)
+            creation_time = creation_time.replace(microsecond=0)
+
+            # print the comparisons between dates
+            print(f"Comparing {creation_time} > {since} = {creation_time > since}")
+
+            # Check if the file was created after the provided `since` date
+            if creation_time > since:
+                filepaths.append(filepath)
+                creation_times.append(creation_time)
+
+    print(f"Found {len(filepaths)} patchnotes since {since}")
+    return filepaths, creation_times
+
+
+def find_patchnotes(since):
+    filepaths, creation_times = get_patchnote_files(since)
+
+    # Sort filepaths based on creation_times, and take the top PATCHNOTE_LIMIT most recent filepaths
+    sorted_filepaths = sorted(zip(filepaths, creation_times), reverse=True)[0:PATCHNOTE_LIMIT]
+
+    # Create a list of dictionaries with the filename and creation time for each patchnote
+    patchnotes = []
+    for (filepath, creation_time) in sorted_filepaths:
+        # Open the file and read its contents
+        with open(filepath, 'r') as f:
+            content = f.read()
+
+        patchnotes.append({
+            'filename': os.path.basename(filepath),
+            'creationTime': str(creation_time),
+            'content': content
+        })
+
+    return patchnotes
+
+
+@app.route('/has-updates', methods=['GET'])
 def has_updates():
+    print("Checking for updates")
     output = subprocess.check_output(["apt", "update"]).decode("utf-8")
-    print(output)
     if " can be upgraded." in output:
         return jsonify(hasUpdates=True)
     else:
         return jsonify(hasUpdates=False)
 
+
 @app.route('/upgrade', methods=['POST'])
 def upgrade():
-    output = subprocess.check_output(["apt", "upgrade", "-y"]).decode("utf-8")
-    print(output)
-    return jsonify(log=output)
+    process = subprocess.Popen(["apt", "upgrade", "-y"])
+    print(f"Starting upgrade with PID {process.pid}")
+    return jsonify(pid=process.pid)
+
+
+@app.route('/pid-exists/<int:pid>', methods=['GET'])
+def pid_exists(pid):
+    is_running = check_pid_running(pid)
+    print("PID {} running?: {}".format(pid, is_running))
+    return jsonify({'exists': is_running})
+
+
+@app.route('/patchnotes', methods=['GET'])
+def get_patchnotes():
+    since_str = request.args.get('since')
+    print(since_str)
+    try:
+        since_date = datetime.fromisoformat(since_str)
+    except:
+        since_date = datetime(1970, 1, 1, tzinfo=timezone.utc)
+    print(since_date.tzinfo)
+
+    print("fetching patchnotes since", since_date)
+    patchnotes = list(find_patchnotes(since_date))
+
+    # Sort patchnotes by creation time in descending order
+    patchnotes.sort(key=lambda x: x['creationTime'], reverse=True)
+
+    return jsonify(patchnotes=patchnotes)
+
 
 @app.route('/')
 def root():
---
+++ web/static/scripts/index.js
@@ -0,0 +1,130 @@
+// GLOBALS
+const ipAddress = window.location.href.replace(/^.*?:\/\//, '').replace(/\/.*/, '').replace(/:.*/, '');
+var checkPid = undefined;
+var pidCheckIntervalId = undefined;
+var newPatchDate = new Date(localStorage.getItem("lastPatchDate"));
+const upgradeWaitingContainer = document.getElementById('upgrade-waiting-container');
+const upgradeResultsContainer = document.getElementById('upgrade-results-container');
+const patchnotesContainer = document.getElementById('patchnotes-container');
+const updateModal = document.getElementById('update-modal');
+const updateButton = document.getElementById('update-button');
+
+// FUNCTIONS
+function fetchPatchnotes(showSystemOnlyUpdates) {
+    console.log("Fetching patch notes...");
+    fetch(`/patchnotes?since=${localStorage.getItem("lastPatchDate")}`)
+        .then(response => response.json())
+        .then(data => {
+            if (!data || !data.patchnotes) {
+                console.info('No data returned from server.');
+                return;
+            }
+
+            // clear patchnotes section
+            while (patchnotesContainer.firstChild) {
+                patchnotesContainer.removeChild(patchnotesContainer.firstChild);
+            }
+
+            const patchnotes = data.patchnotes; // type [{filename: string, content: string, creationTime: string}]
+
+            if (patchnotes.length > 0) {
+                patchnotes.forEach((item) => {
+                    console.log("Processing item:", item);
+                    const header = document.createElement('h2');
+                    const filenameParts = item.filename.split('-'); // Split the string at '-'
+                    const nameVersionParts = filenameParts[1].split('.') // Split the second part of the split string at '.'
+                    const headerText = `${filenameParts[0].replace('_', ' ')} v${nameVersionParts[0]}.${nameVersionParts[1]}`; // Replace '_' with ' ', and construct the final string
+                    header.textContent = headerText;
+                    header.style.alignSelf = "start";
+
+                    const textarea = document.createElement('textarea');
+                    textarea.title = "Patch Notes";
+                    textarea.textContent = item.content;
+                    textarea.style.width = '600px';
+                    textarea.style.height = '400px'; // Adjust the height as needed for your design
+                    textarea.style.marginBottom = '12px'; // Add some space below each textarea
+                    textarea.readOnly = true;
+
+                    patchnotesContainer.appendChild(header);
+                    patchnotesContainer.appendChild(textarea);
+                });
+
+                newPatchDate = new Date(patchnotes[0].creationTime);
+            } else {
+                const p = document.createElement('p');
+                p.textContent = "No additional patch notes are available."
+                patchnotesContainer.appendChild(p);
+            }
+
+            if (patchnotes.length > 0 || showSystemOnlyUpdates) {
+                // Show the patch notes
+                upgradeWaitingContainer.style.display = 'none';
+                upgradeResultsContainer.style.display = 'flex';
+                updateModal.style.display = 'flex';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function checkPidExists() {
+    if (typeof checkPid !== 'number') {
+        clearInterval(pidCheckIntervalId);
+        return;
+    }
+
+    fetch(`/pid-exists/${checkPid}`)
+        .then(response => response.json())
+        .then(data => {
+            if (!data.exists) {
+                clearInterval(pidCheckIntervalId);
+                fetchPatchnotes(true);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+// ON LOAD
+fetchPatchnotes(false);
+
+fetch('/has-updates')
+    .then(response => response.json())
+    .then(data => {
+        const hasUpdates = data.hasUpdates;
+        updateButton.style.display = hasUpdates ? 'block' : 'none';
+    })
+    .catch((error) => console.error(error));
+
+document.querySelector('.openwebui').href = 'http://' + ipAddress + ':8080';
+document.querySelector('.stableswarmui').href = 'http://' + ipAddress + ':7801';
+
+updateButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    fetch('/upgrade', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({}),
+    })
+        .then((response) => response.json())
+        .then(data => {
+            checkPid = data.pid;
+            pidCheckIntervalId = setInterval(checkPidExists, 1000);
+            upgradeWaitingContainer.style.display = 'flex';
+            upgradeResultsContainer.style.display = 'none';
+            updateModal.style.display = 'flex';
+            updateButton.style.display = 'none';
+        })
+        .catch((error) => console.error(error));
+});
+document.getElementById('close-button').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    localStorage.setItem("lastPatchDate", newPatchDate.toISOString());
+
+    upgradeWaitingContainer.style.display = 'none';
+    upgradeResultsContainer.style.display = 'none';
+    updateModal.style.display = 'none';
+    window.location.reload();
+});
